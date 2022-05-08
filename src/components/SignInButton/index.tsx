2483694c2552c1c30435755226dc3b06b069cb3b
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-07 22:23:36 -0300

    authentication with Github created

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,34 @@
 import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+// signIn é a função que faz autenticação do usuário.
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export const SignInButton = () => {
   const isUserLoggedIn = true
+  // Estou desestruturando data de useSession e renomeando ela para session.
+  const { data: session } = useSession()
 
-  return isUserLoggedIn ? (
+  console.log('session:', session)
+
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
     >
       <FaGithub color="#04D361" />
-      JhonatanGAlves
-      <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name}
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#EBA417" />
       Sign in with Github
