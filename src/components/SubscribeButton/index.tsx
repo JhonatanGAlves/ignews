386063795d5d0c0e307584ff30238c4c5f2bb09a
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-13 00:25:06 -0300

    make all logic in SubscribeButton

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,10 +1,39 @@
+import { signIn, useSession } from 'next-auth/react'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
-export const SubscribeButton = () => {
+type SubscribeButtonProps = {
+  priceId: string
+}
+
+export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const { data: session } = useSession()
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe') // Nome do arquivo eé sempre o nome da rota.
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
